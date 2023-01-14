85d4ab6c0c8619f21f4153cb50b72472f1f3f678
Ramilya1 <heypersik@yandex.ru>
2023-01-14 20:06:13 +0500

    first

--- js/script.js
+++ js/script.js
@@ -1,14 +1,46 @@
-var swiper = new Swiper('.swiper-container',{
-    centeredSlides: true,
-    effect: 'coverflow',
-    grabCursor: true,
-    slidersPerView: 'auto',
-    spaceBetween: 0,
+var swiper = new Swiper('.gallery',{
+    //стрелки
     loop: true,
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 0,
-        modifier: 1,
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
     },
+    //точки указатели
+    //pagination: {
+        //el: '.swiper-pagination',
+        //clickable: true
+    //},
+    
+    //scrollbar: {
+        //el: '.swiper-scrollbar',
+        //draggable: true
+    //},
+    touchRatio: 1,
+    //переключение клавиатурой
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+    //скролмышью
+    mousewheel: {   
+        sensitivity: 1,
+        //eventsTarget: '.gallery'
+    },
+
+
+    //centeredSlides: true,
+    //effect: 'coverflow',
+    //grabCursor: true,
+    //slidersPerView: 'auto',
+    //spaceBetween: 0,
+    //loop: true,
+
+    //coverflowEffect: {
+        //rotate: 0,
+        //stretch: 0,
+        //depth: 0,
+        //modifier: 1
+    //},
+
 });
