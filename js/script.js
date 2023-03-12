777d28cd2795bb227de3a5c9ef9bb62cf4c05327
Ramilya1 <heypersik@yandex.ru>
2023-03-12 22:26:17 +0500

    1203

--- js/script.js
+++ js/script.js
@@ -44,3 +44,53 @@ var swiper = new Swiper('.gallery',{
     //},
 
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+    const form2 = document.getElementById('form2');
+    form2.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form2);
+    }
+
+    function formValidate(form2) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')){
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+
+            }else if(input.getAttribute("type") === "checkbox" && input.checked === false){
+                formAddError(input);
+                error++;
+            }else{
+                if (input.value === ''){
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    //email
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2.8})+$/.test(input.value);
+    }
+});
